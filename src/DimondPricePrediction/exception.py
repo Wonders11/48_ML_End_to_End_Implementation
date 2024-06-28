f4527066d4db2c16fcfbe513d2e79b8d35d4b93d
Mohit_100 <mohithedaoo2000@gmail.com>
2024-06-28 08:20:55 +0530

    Exception module added

---
+++ src/DimondPricePrediction/exception.py
@@ -0,0 +1,24 @@
+import sys
+
+class customexception(Exception): # for fetching details like file name, line number we are using this method
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        # In which file,which line number and error message we are getting error 
+        _,_,exc_tb = error_details.exc_info() # exc_tb is execution traceback
+
+        self.lineno = exc_tb.tb_lineno # getting line number
+        self.file_name = exc_tb.tb_frame.f_code.co_filename # getting file name 
+
+    def __str__(self): # for printing message we are using this method
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name,self.lineno,str(self.error_message)
+        )
+    
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+
+    except Exception as e:
+        raise customexception(e,sys)
+
