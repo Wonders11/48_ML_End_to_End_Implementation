3fe58d04adc358c86b326bd6a935221235754fdb
Mohit_100 <mohithedaoo2000@gmail.com>
2024-06-29 07:42:32 +0530

    Added exception code and updated research.ipynb

--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -4,6 +4,7 @@ class customexception(Exception): # for fetching details like file name, line nu
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
         # In which file,which line number and error message we are getting error 
+        # In below line _,_, refers to 2 variables as outcome from exc_info which are not important and can be ignored
         _,_,exc_tb = error_details.exc_info() # exc_tb is execution traceback
 
         self.lineno = exc_tb.tb_lineno # getting line number
