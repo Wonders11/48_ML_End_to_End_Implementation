de8f53c33c2e67c7c374b88689f91cb2d6c5af12
Mohit_100 <mohithedaoo2000@gmail.com>
2024-07-01 17:35:09 +0530

    Written code for front end

---
+++ app.py
@@ -0,0 +1,48 @@
+# Creation of WebAPI using Flask
+# By default Flask runs on 5000 port number
+
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
+
+from flask import Flask,request,render_template,jsonify
+
+# creating object of flask
+app = Flask(__name__)
+
+# creating routes
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+# open browser and search localhost:5000 and it will show o/p
+
+@app.route('/predict',methods=["GET","POST"])
+def predict_datapoint(): # code for prediction will be here
+    if request.method == "GET":
+        return render_template("form.html")
+
+    else:
+        data=CustomData(
+            carat=float(request.form.get("carat")),
+            depth=float(request.form.get("depth")),
+            table=float(request.form.get("table")),
+            x=float(request.form.get("x")),
+            y=float(request.form.get("y")),
+            z=float(request.form.get("z")),
+            cut=request.form.get("cut"),
+            color=request.form.get("color"),
+            clarity=request.form.get("clarity")
+        )
+
+        final_data = data.get_data_as_dataframe()
+
+        predict_pipeline = PredictPipeline()
+
+        pred = predict_pipeline.predict(final_data)
+
+        result = round(pred[0],2)
+
+        return render_template("results.html",final_result=result)
+
+if __name__ ==  '__main__':
+    app.run() # without this we won't get the output
+
